433a708353b43e1e3aef2ce9d4e4d97a51656a35
Mohamad Sleiman Haidar <mhaidar@myki.com>
2022-12-28 13:53:09 +0300

    disable user indexing on user creation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import 'reflect-metadata'
 // import 'dotenv-safe/config'
 
--- src/subscribers/UserSubscriber.ts
+++ src/subscribers/UserSubscriber.ts
@@ -37,7 +37,7 @@ export class UserSubscriber implements EntitySubscriberInterface<User> {
         await getConnection().manager.save(user)
 
         await createUserAndAssociateWithProfile(user, profile.raw[0])
-        await rpcClient.search().indexProfile({ profile })
+        // await rpcClient.search().indexProfile({ profile })
       }
     } catch (e) {
       console.log('error in listener:', e)
