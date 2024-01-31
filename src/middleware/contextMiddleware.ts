a6fdab8a537143eec97688dadc12504173ac7d08
msh <mohamad.sleimanhaidar@gmail.com>
2024-01-31 15:14:48 +0200

    disable middelware

--- src/middleware/contextMiddleware.ts
+++ src/middleware/contextMiddleware.ts
@@ -1,28 +1,28 @@
-// @ts-nocheck
-import { getIO } from '../socketio/socket'
-import { NextFunction } from 'express'
-import { createUpdootLoader } from './../utils/createUpdootLoader'
-import { createUserLoader } from './../utils/createUserLoader'
-import { createMessageLoader } from './../utils/createMessageLoader'
-
-export function contextMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const io = getIO() // Get the Socket.IO instance
-
-    req.context = {
-      userLoader: createUserLoader(),
-      updootLoader: createUpdootLoader(),
-      messageLoader: createMessageLoader(),
-      io, // Attach the Socket.IO instance to the context
-    }
-
-    next()
-  } catch (error) {
-    console.error('error in context:', error)
-    res.status(500).send('Internal Server Error')
-  }
-}
+// // @ts-nocheck
+// import { getIO } from '../socketio/socket'
+// import { NextFunction } from 'express'
+// import { createUpdootLoader } from './../utils/createUpdootLoader'
+// import { createUserLoader } from './../utils/createUserLoader'
+// import { createMessageLoader } from './../utils/createMessageLoader'
+//
+// export function contextMiddleware(
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) {
+//   try {
+//     const io = getIO() // Get the Socket.IO instance
+//
+//     req.context = {
+//       userLoader: createUserLoader(),
+//       updootLoader: createUpdootLoader(),
+//       messageLoader: createMessageLoader(),
+//       io, // Attach the Socket.IO instance to the context
+//     }
+//
+//     next()
+//   } catch (error) {
+//     console.error('error in context:', error)
+//     res.status(500).send('Internal Server Error')
+//   }
+// }
--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,11 +1,11 @@
-// @ts-nocheck
-import { MyContext } from 'src/types'
-import { MiddlewareFn } from 'type-graphql'
-
-export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
-  if (!context.req.session.userId) {
-    throw new Error('not authenticated')
-  }
-
-  return next()
-}
+// // @ts-nocheck
+// import { MyContext } from 'src/types'
+// import { MiddlewareFn } from 'type-graphql'
+//
+// export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+//   if (!context.req.session.userId) {
+//     throw new Error('not authenticated')
+//   }
+//
+//   return next()
+// }
