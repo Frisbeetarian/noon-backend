372733bba2768f1154fa5d66a865c8ded1e89486
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-02-09 11:58:26 +0200

    validate file uploads

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,9 +1,25 @@
 import express from 'express'
-import MessageController from '../controllers/MessageController'
 import multer from 'multer'
+import path from 'path'
+
+import MessageController from '../controllers/MessageController'
 
 const messageRouter = express.Router()
-const upload = multer({ storage: multer.memoryStorage() })
+
+const allowedExtensions = /\.(jpg|jpeg|png|doc|docx|pdf|ogg)$/i
+const fileSizeLimit = 1 * 1024 * 1024 // 1MB
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: fileSizeLimit },
+  fileFilter: (req, file, cb) => {
+    if (!allowedExtensions.test(path.extname(file.originalname))) {
+      req.fileValidationError = 'Forbidden extension'
+      return cb(null, false, req.fileValidationError)
+    }
+    cb(null, true)
+  },
+})
 
 messageRouter.get(
   '/:conversationUuid',
@@ -13,12 +29,34 @@ messageRouter.get(
 messageRouter.post(
   '/uploadFile',
   upload.single('file'),
+  (req, res, next) => {
+    if (req.fileValidationError) {
+      return res.status(400).json({ error: req.fileValidationError })
+    } else if (!req.file) {
+      return res.status(400).json({ error: 'No file provided' })
+    } else if (req.file.size > fileSizeLimit) {
+      // This check is redundant due to multer's limits, but included for clarity
+      return res.status(400).json({ error: 'File is too large' })
+    }
+    next()
+  },
   MessageController.saveFile
 )
 
 messageRouter.post(
   '/uploadVoiceRecording',
   upload.single('file'),
+  (req, res, next) => {
+    if (req.fileValidationError) {
+      return res.status(400).json({ error: req.fileValidationError })
+    } else if (!req.file) {
+      return res.status(400).json({ error: 'No file provided' })
+    } else if (req.file.size > fileSizeLimit) {
+      // This check is redundant due to multer's limits, but included for clarity
+      return res.status(400).json({ error: 'File is too large' })
+    }
+    next()
+  },
   MessageController.saveVoiceRecording
 )
 messageRouter.post('/groupMessages', MessageController.handleGroupMessage)
