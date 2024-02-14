2ebc3a31fd8a45339791205b8bf7a8af960cb39f
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-02-14 11:54:43 +0200

    fix: align rest of limiters

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -24,58 +24,62 @@ export const messageLimiter = rateLimit({
   },
 })
 
-// export const globalLimiter = rateLimit({
-//   windowMs: 60 * 1000, // 1 minute
-//   limit: 50,
-//   standardHeaders: true, // Return 'RateLimit-*' headers in the response
-//   legacyHeaders: false, // Disable X-RateLimit-* headers
-//   store: new RedisStore({
-//     client: redisClient,
-//     prefix: 'global_limiter_',
-//   }),
-//   handler: (_req, res, _next, options) => {
-//     res.status(options.statusCode).json({
-//       error: options.message,
-//       retryAfter: Math.ceil(options.windowMs / 1000),
-//     })
-//   },
-//   skip: (req) =>
-//     [
-//       '/api/messages/handleMessage',
-//       '/api/messages/handleGroupMessage',
-//       '/api/users/register',
-//       '/api/users/login',
-//     ].includes(req.baseUrl),
-// })
-//
-// export const loginLimiter = rateLimit({
-//   windowMs: 10 * 60 * 1000, // 10 minutes
-//   limit: 5,
-//   store: new RedisStore({
-//     client: redisClient,
-//     prefix: 'login_limiter_',
-//   }),
-//   message: 'Too many login attempts. Please try again later.',
-//   handler: (_req, res, _next, options) => {
-//     res.status(options.statusCode).json({
-//       error: options.message,
-//       retryAfter: Math.ceil(options.windowMs / 1000),
-//     })
-//   },
-// })
-//
-// export const registerLimiter = rateLimit({
-//   windowMs: 60 * 60 * 1000, // 1 hour
-//   limit: 5,
-//   store: new RedisStore({
-//     client: redisClient,
-//     prefix: 'register_limiter_',
-//   }),
-//   message: 'Too many registration attempts. Please try again later.',
-//   handler: (_req, res, _next, options) => {
-//     res.status(options.statusCode).json({
-//       error: options.message,
-//       retryAfter: Math.ceil(options.windowMs / 1000),
-//     })
-//   },
-// })
+export const globalLimiter = rateLimit({
+  ...defaultSettings,
+  windowMs: 60 * 1000, // 1 minute
+  limit: 50,
+  store: new RedisStore({
+    // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+    sendCommand: (...args: string[]) => redisClient.call(...args),
+    prefix: 'global_limiter_',
+  }),
+  handler: (_req, res, _next, options) => {
+    res.status(options.statusCode).json({
+      error: options.message,
+      retryAfter: Math.ceil(options.windowMs / 1000),
+    })
+  },
+  skip: (req) =>
+    [
+      '/api/messages/handleMessage',
+      '/api/messages/handleGroupMessage',
+      '/api/users/register',
+      '/api/users/login',
+    ].includes(req.baseUrl),
+})
+
+export const loginLimiter = rateLimit({
+  ...defaultSettings,
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 5,
+  store: new RedisStore({
+    // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+    sendCommand: (...args: string[]) => redisClient.call(...args),
+    prefix: 'login_limiter_',
+  }),
+  message: 'Too many login attempts. Please try again later.',
+  handler: (_req, res, _next, options) => {
+    res.status(options.statusCode).json({
+      error: options.message,
+      retryAfter: Math.ceil(options.windowMs / 1000),
+    })
+  },
+})
+
+export const registerLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  ...defaultSettings,
+  limit: 5,
+  store: new RedisStore({
+    // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+    sendCommand: (...args: string[]) => redisClient.call(...args),
+    prefix: 'register_limiter_',
+  }),
+  message: 'Too many registration attempts. Please try again later.',
+  handler: (_req, res, _next, options) => {
+    res.status(options.statusCode).json({
+      error: options.message,
+      retryAfter: Math.ceil(options.windowMs / 1000),
+    })
+  },
+})
